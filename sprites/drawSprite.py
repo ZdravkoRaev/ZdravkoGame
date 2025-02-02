7017935f229277974739023621dc59df463d62ac
ZdravkoRaev <zdr.raev@gmail.com>
2025-02-02 18:25:57 +0200

    testing drawing a sprite

--- sprites/drawSprite.py
+++ sprites/drawSprite.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 def draw(ID : int, x : int, y : int,surface):
-    image=pygame.image.load("C:\pythonstuff\homework7\sprites\wallsprites\wall0.png").convert_alpha()
+    image=pygame.image.load(os.path.join("sprites","enemies","target.png")).convert_alpha()
     surface.blit(image,(x,y))
