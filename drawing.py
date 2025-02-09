58f0c7985af9ffee5d5850ded62cf178fb9a57d0
ZdravkoRaev <zdr.raev@gmail.com>
2025-02-09 18:37:10 +0200

    added player sprites

--- drawing.py
+++ drawing.py
@@ -54,24 +54,74 @@ def draw(screen,player : Player ,level,enemies,frame:int):
     walk4=pygame.image.load(os.path.join("sprites","player","walking","walk4.png")).convert_alpha()
     walk5=pygame.image.load(os.path.join("sprites","player","walking","walk5.png")).convert_alpha()
     walk6=pygame.image.load(os.path.join("sprites","player","walking","walk6.png")).convert_alpha()
-    
+
+    stand0=pygame.image.load(os.path.join("sprites","player","standing","stand0.png")).convert_alpha()
+    stand1=pygame.image.load(os.path.join("sprites","player","standing","stand1.png")).convert_alpha()
+    stand2=pygame.image.load(os.path.join("sprites","player","standing","stand2.png")).convert_alpha()
+    stand3=pygame.image.load(os.path.join("sprites","player","standing","stand3.png")).convert_alpha()
+
+    jump0=pygame.image.load(os.path.join("sprites","player","jumping","jump0.png")).convert_alpha()
+    jump1=pygame.image.load(os.path.join("sprites","player","jumping","jump1.png")).convert_alpha()
+    jump2=pygame.image.load(os.path.join("sprites","player","jumping","jump2.png")).convert_alpha()
+
     pygame.display.update()
     screen.fill((0,0,0))
     if player.hp<=0:
         pygame.draw.rect(screen,(0,100,0),player.base.bounding_box)
     else:
-        if int(frame/3)%6==0:
-            screen.blit(walk1,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
-        elif int(frame/3)%6==1:
-            screen.blit(walk2,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
-        elif int(frame/3)%6==2:
-            screen.blit(walk3,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
-        elif int(frame/3)%6==3:
-            screen.blit(walk4,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
-        elif int(frame/3)%6==4:
-            screen.blit(walk5,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
-        else: 
-            screen.blit(walk6,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+        if player.base.wall_below:
+            if player.base.x_vel>0.1:
+                if int(frame/3)%6==0:
+                    screen.blit(walk1,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                elif int(frame/3)%6==1:
+                    screen.blit(walk2,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                elif int(frame/3)%6==2:
+                    screen.blit(walk3,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                elif int(frame/3)%6==3:
+                    screen.blit(walk4,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                elif int(frame/3)%6==4:
+                    screen.blit(walk5,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                else:
+                    screen.blit(walk6,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+            elif player.base.x_vel<-0.1:
+                if int(frame/3)%6==0:
+                    walkcopy=walk1.copy()
+                    walkcopy = pygame.transform.flip(walkcopy, True, False)
+                elif int(frame/3)%6==1:
+                    walkcopy=walk2.copy()
+                    walkcopy = pygame.transform.flip(walkcopy, True, False)
+                elif int(frame/3)%6==2:
+                    walkcopy=walk3.copy()
+                    walkcopy = pygame.transform.flip(walkcopy, True, False)
+                elif int(frame/3)%6==3:
+                    walkcopy=walk4.copy()
+                    walkcopy = pygame.transform.flip(walkcopy, True, False)
+                elif int(frame/3)%6==4:
+                    walkcopy=walk5.copy()
+                    walkcopy = pygame.transform.flip(walkcopy, True, False)
+                else:
+                    walkcopy=walk6.copy()
+                    walkcopy = pygame.transform.flip(walkcopy, True, False)
+                screen.blit(walkcopy,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+            else:
+                if int(frame/12)%4==0:
+                    screen.blit(stand0,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                elif int(frame/12)%4==1:
+                    screen.blit(stand1,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+                elif int(frame/12)%4==2:
+                    screen.blit(stand2,(player.base.bounding_box.x-9,player.base.bounding_box.y-12))
+                elif int(frame/12)%4==3:
+                    screen.blit(stand3,(player.base.bounding_box.x-9,player.base.bounding_box.y-12))
+        else:
+            if int(frame/6)%3==0:
+                screen.blit(jump0,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+            elif int(frame/6)%3==1:
+                screen.blit(jump1,(player.base.bounding_box.x-7,player.base.bounding_box.y-12))
+            elif int(frame/6)%3==2:
+                screen.blit(jump2,(player.base.bounding_box.x-9,player.base.bounding_box.y-12))
+
+
+
     for item in enemies:
         color=(255,255,255)
         if item.id==1:
--- enteties/player/player.py
+++ enteties/player/player.py
@@ -6,13 +6,13 @@ class Player:
     def __init__(self):
         self.base=BaseEntety(25,50)
         self.wall_jump_box=BaseEntety(27,48)
-
         self.hp=3
         self.i_frames=30
-
         self.attack_cd=0
         self.cad_dash=False
         self.is_dashing=False
         self.attack_lenght=3
         self.hurtbox=Rect((150,150,75,75))
         self.is_attacking=False
+
+        #add sprites here
